084e5e845fd1ad9f6fc6b0c0a5e6f289c42e8690
stefbou9 <stefbou9@gmail.com>
2018-03-30 13:35:57 +0300

    Create Grades.java

---
+++ ergasia1-2018/src/Grades.java
@@ -0,0 +1,33 @@
+
+
+public class Grades {
+
+	private Students student;
+	private Professors professor;
+	private Courses course;
+	
+	
+	public Students getStudent() {
+		return student;
+	}
+	public void setStudent(Students student) {
+		this.student = student;
+	}
+	
+	
+	public Professors getProfessor() {
+		return professor;
+	}
+	public void setProfessor(Professors professor) {
+		this.professor = professor;
+	}
+	
+	
+	public Courses getCourse() {
+		return course;
+	}
+	public void setCourse(Courses course) {
+		this.course = course;
+	}
+	
+}
